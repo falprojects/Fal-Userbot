7a2e8fd76ec564b2dcf6a5aad90bf97bb8f064ce
falprojects <101349924+falprojects@users.noreply.github.com>
2022-04-09 01:43:59 +0800

    Update and rename kyyhelper.py to helper.py

---
+++ userbot/modules/helper.py
@@ -0,0 +1,30 @@
+""" Userbot module for other small commands. """
+from userbot import CMD_HELP, owner, CMD_HANDLER as cmd
+from userbot.utils import edit_or_reply, fal_cmd
+
+
+@fal_cmd(pattern="ihelp$")
+async def usit(e):
+    await edit_or_reply(e,
+                        f"**Hai {me.first_name} Kalo Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"       
+        f"❆ **Channel Fal :** [N](t.me/fallprojects)\n"
+        f"❆ **Owner Repo :** [N](t.me/falprojects)\n"
+        f"❆ **Repo :** [Fal-Userbot](https://github.com/falprojects/Fal-Userbot)\n",
+        )
+
+                        
+@fal_cmd(pattern="vars$")
+async def var(m):
+    await edit_or_reply(m,
+                        f"**Disini Daftar Vars Dari {owner}:**\n"
+                        "\n[DAFTAR VARS](https://raw.githubusercontent.com/falprojects/Fal-Userbot/Fal-Userbot/varshelper.txt)",
+        )
+
+
+CMD_HELP.update({
+    "helper":
+    f"`{cmd}ihelp`\
+\nUsage: Bantuan Untuk Fal-Userbot.\
+\n`{cmd}vars`\
+\nUsage: Melihat Daftar Vars."
+})
