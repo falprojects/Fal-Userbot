4eec04fc28753e693a1c497e416674e9a32ee939
falprojects <jukira730@gmail.com>
2022-04-08 17:44:55 +0000

    pylint: auto fixes
    Signed-off-by: falprojects <jukira730@gmail.com>

--- userbot/modules/helper.py
+++ userbot/modules/helper.py
@@ -6,19 +6,19 @@ from userbot.utils import edit_or_reply, fal_cmd
 @fal_cmd(pattern="ihelp$")
 async def usit(e):
     await edit_or_reply(e,
-                        f"**Hai {me.first_name} Kalo Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"       
-        f"❆ **Channel Fal :** [N](t.me/fallprojects)\n"
-        f"❆ **Owner Repo :** [N](t.me/falprojects)\n"
-        f"❆ **Repo :** [Fal-Userbot](https://github.com/falprojects/Fal-Userbot)\n",
-        )
+                        f"**Hai {me.first_name} Kalo Anda Tidak Tau Perintah Untuk Memerintah Ku Ketik** `.help` Atau Bisa Minta Bantuan Ke:\n"
+                        f"❆ **Channel Fal :** [N](t.me/fallprojects)\n"
+                        f"❆ **Owner Repo :** [N](t.me/falprojects)\n"
+                        f"❆ **Repo :** [Fal-Userbot](https://github.com/falprojects/Fal-Userbot)\n",
+                        )
 
-                        
+
 @fal_cmd(pattern="vars$")
 async def var(m):
     await edit_or_reply(m,
                         f"**Disini Daftar Vars Dari {owner}:**\n"
                         "\n[DAFTAR VARS](https://raw.githubusercontent.com/falprojects/Fal-Userbot/Fal-Userbot/varshelper.txt)",
-        )
+                        )
 
 
 CMD_HELP.update({
