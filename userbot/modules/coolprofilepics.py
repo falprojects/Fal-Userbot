543b1b61acecd75c657a15d87a4c6e778ab2a1bd
falprojects <101349924+falprojects@users.noreply.github.com>
2022-04-12 12:28:50 +0800

    Update coolprofilepics.py

--- userbot/modules/coolprofilepics.py
+++ userbot/modules/coolprofilepics.py
@@ -81,5 +81,5 @@ async def main(event):
         await asyncio.sleep(3600)  # Edit this to your required needs
 
 CMD_HELP.update({
-    "randompp": "\n\n✨𝘾𝙈𝘿✨: `.pprandom`"
+    "randompp": "\n\n⚡𝘾𝙈𝘿⚡: `.pprandom`"
     "\n↳ : Mengubah Photo Profile Anda Secara Random."})
